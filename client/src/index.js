95afb70cefdb3de2ec54030a969b64240c08393b
Nipun <shehanatuk@gmail.com>
2022-01-08 20:51:10 -0500

    added environment.js file to /frontend/src

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,18 @@ import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import redux from "./redux/store";
 import App from './App';
+import { HttpLink } from '@apollo/client';
+import { GRAPHQL_HTTP_SERVER_URL, GRAPHQL_WS_SERVER_URL } from './environment';
+
+// Creating a HTTP link 
+const httpLink = new HttpLink({
+  uri: GRAPHQL_HTTP_SERVER_URL
+});
+
+const wsLink = new WebSocketLink({
+  uri: GRAPHQL_WS_SERVER_URL, 
+  options: { reconnect: true },
+});
 
 ReactDOM.render(
   <Provider store={redux.store}>
